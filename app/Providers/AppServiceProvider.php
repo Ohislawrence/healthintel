739b8f5dad64a9167ab97d9fd1e7b106123d5c29
Ohislawrence <lawrenceohis@gmail.com>
2026-07-23 20:34:40 +0100

    api errors1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,9 +34,6 @@ class AppServiceProvider extends ServiceProvider
             );
         });
 
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
     }
 }
 
